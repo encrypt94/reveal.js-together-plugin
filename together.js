c738286dcbde5c887337d21c49348d13b52199ee
encrypt <encrypt94@gmail.com>
2015-04-08 08:50:36 +0200

    first commit

---
+++ together.js
@@ -0,0 +1,25 @@
+document.addEventListener('keydown', function(e){
+    var evtObj = window.event? event : e
+    if(evtObj.keyCode == 84 && evtObj.altKey && evtObj.ctrlKey) {
+	TogetherJS(this);
+	return false;
+    }
+},false);
+
+Reveal.addEventListener("slidechanged", function(event){
+    if(TogetherJS.require("peers").Self.isCreator) {
+	TogetherJS.send({
+	    type: "slidechanged",
+	    indexh: event.indexh,
+	    indexv: event.indexv
+	});
+    }
+});
+
+TogetherJS.hub.on("slidechanged", function(event){
+    console.log("event received");
+    if(event.peer.isCreator) {
+	console.log(event);
+	Reveal.slide(event.indexh, event.indexv);
+    }
+});
