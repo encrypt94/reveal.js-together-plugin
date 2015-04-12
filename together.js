903ab6531ba36edfa81ec4ca76f327334c291479
encrypt <encrypt94@gmail.com>
2015-04-12 16:11:44 +0200

    add fragment sync and code restyle

--- together.js
+++ together.js
@@ -1,25 +1,50 @@
-document.addEventListener('keydown', function(e){
-    var evtObj = window.event? event : e
-    if(evtObj.keyCode == 84 && evtObj.altKey && evtObj.ctrlKey) {
-	TogetherJS(this);
-	return false;
-    }
-},false);
-
-TogetherJS.on("ready", function(){
-    Reveal.addEventListener("slidechanged", function(event){
+window.togetherPlugin = {
+    sendSlideChanged: function(h, v, f){
 	if(TogetherJS.require("peers").Self.isCreator) {
 	    TogetherJS.send({
 		type: "slidechanged",
-		indexh: event.indexh,
-		indexv: event.indexv
+		indexh: h,
+		indexv: v,
+		indexf: f
 	    });
 	}
-    });
+    },
 
-    TogetherJS.hub.on("slidechanged", function(event){
-	if(event.peer.isCreator) {
-	    Reveal.slide(event.indexh, event.indexv);
-	}
-    });
-})
+    sendFragmentChanged: function(){
+	var indices = Reveal.getIndices();
+	this.sendSlideChanged(indices.h, indices.v, indices.f);
+    },
+
+    init: function(){
+	document.addEventListener('keydown', function(e){
+	    var evtObj = window.event? event : e
+	    // Ctrl + Alt + t
+	    if(evtObj.keyCode == 84 && evtObj.altKey && evtObj.ctrlKey) {
+		TogetherJS(this);
+		return false;
+	    }
+	},false);
+
+	TogetherJS.on("ready", function(){
+	    Reveal.addEventListener("slidechanged", function(event){
+		togetherPlugin.sendSlideChanged(event.indexh, event.indexv, 0)
+	    });
+
+	    Reveal.addEventListener("fragmentshown", function(event){
+		togetherPlugin.sendFragmentChanged();
+	    });
+
+	    Reveal.addEventListener("fragmenthidden", function(event){
+		togetherPlugin.sendFragmentChanged();
+	    });
+
+	    TogetherJS.hub.on("slidechanged", function(event){
+		if(event.peer.isCreator) {
+		    Reveal.slide(event.indexh, event.indexv, event.indexf);
+		}
+	    });
+	});
+    }
+}
+
+togetherPlugin.init();
